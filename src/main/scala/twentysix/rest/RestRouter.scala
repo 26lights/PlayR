3c1de41fd7414b22c9d14d3bdc4f65ea7e39f07a
Olivier Samyn <olivier@26lights.com>
2013-12-17 16:22:36 +0100

    Optimize controller feature detection.

--- src/main/scala/twentysix/rest/RestRouter.scala
+++ src/main/scala/twentysix/rest/RestRouter.scala
@@ -8,9 +8,9 @@ import play.api.mvc.Handler
 import play.api.mvc.Controller
 import play.api.Logger
 import play.api.mvc.Controller
+import scala.reflect._
 
-trait RestRouter[C <:Controller] extends Router.Routes {
-  def controller: C
+class RestRouter(val controller: Controller) extends Router.Routes {
 
   protected var _prefix: String =""
 
@@ -21,119 +21,86 @@ trait RestRouter[C <:Controller] extends Router.Routes {
   def prefix = _prefix
   def documentation= Nil
 
-  trait RoutingHandler {
-    def isDefined(requestHeader: RequestHeader): Boolean
-    def apply[A <: RequestHeader, B>: Handler]( requestHeader: A, default: A => B): B
-  }
-
-  object DefaultRoutingHandler extends RoutingHandler {
-    def isDefined(requestHeader: RequestHeader) = false
-    def apply[A <: RequestHeader, B>: Handler]( requestHeader: A, default: A => B): B = default(requestHeader)
-  }
-
   private val IdExpression = "^/([^/]+)/?$".r
   private val SubResourceExpression = "^(/([^/]+)/([^/]+)).*$".r
 
-  abstract class IdRoutingHandler[Id, T <:IdentifiedResource[Id]] extends RoutingHandler {
-    def resource: T
-    def isDefined(requestHeader: RequestHeader) = {
-      requestHeader.path.drop(_prefix.length()) match {
-        case "" | "/" => true
-        case IdExpression(id) => true
-        case _ => false
-      }
-    }
+  def _defaultRoutingHandler() = None
+  def _defaultIdRoutingHandler(id: String) = None
+  def _defaultSubRoutingHandler(requestHeader: RequestHeader, subPrefix: String, id: String, subPath: String) = None
 
-    def apply[A <: RequestHeader, B>: Handler]( requestHeader: A, default: A => B): B = {
-      requestHeader.path.drop(_prefix.length()) match {
-        case "" | "/" => withoutId(requestHeader, default)
-        case IdExpression(id) => resource.fromId(id) match {
-          case Some(id) => withId(id, requestHeader, default)
-          case _ => default(requestHeader)
+  def _getRoutingHandler[Id](resource: ResourceRead[Id])(sid: String) = resource.fromId(sid).map { resource.get(_) }
+  def _listRoutingHandler[Id](resource: ResourceRead[Id])() = Some(resource.list)
+  def _putRoutingHandler[Id](resource: ResourceOverwrite[Id])(sid: String) = resource.fromId(sid).map { resource.put(_) }
+  def _deleteRoutingHandler[Id](resource: ResourceUpdate[Id])(sid: String) = resource.fromId(sid).map { resource.delete(_) }
+  def _patchRoutingHandler[Id](resource: ResourceUpdate[Id])(sid: String) = resource.fromId(sid).map { resource.update(_) }
+  def _postRoutingHandler[Id](resource: ResourceAction)() = Some(resource.post)
+  def _subRoutingHandler[Id](resource: SubResource[Id])(requestHeader: RequestHeader, subPrefix: String, sid: String, subPath: String) = {
+    for {
+      f <- resource.subResources.get(subPath)
+      id <- resource.fromId(sid)
+      res <- Router.Include {
+        val router = new RestRouter(f(id))
+        router.setPrefix(requestHeader.path.take(_prefix.length()+subPrefix.length()))
+        router
+      }.unapply(requestHeader)
+    } yield res
+  }
+
+  def controllerAs[A: ClassTag]: Option[A] = {
+    if(classTag[A].runtimeClass.isInstance(controller)) {
+      Some(controller.asInstanceOf[A])
+    } else {
+      None
+    }
+  }
+
+  val getRoutingHandler = controllerAs[ResourceRead[_]].map( _getRoutingHandler(_) _).getOrElse(_defaultIdRoutingHandler _)
+  val listRoutingHandler = controllerAs[ResourceRead[_]].map( _listRoutingHandler(_) _).getOrElse(_defaultRoutingHandler _)
+  val putRoutingHandler = controllerAs[ResourceOverwrite[_]].map( _putRoutingHandler(_) _).getOrElse(_defaultIdRoutingHandler _)
+  val patchRoutingHandler = controllerAs[ResourceUpdate[_]].map( _patchRoutingHandler(_) _).getOrElse(_defaultIdRoutingHandler _)
+  val deleteRoutingHandler = controllerAs[ResourceUpdate[_]].map( _deleteRoutingHandler(_) _).getOrElse(_defaultIdRoutingHandler _)
+  val postRoutingHandler = controllerAs[ResourceAction].map( _postRoutingHandler(_) _).getOrElse(_defaultRoutingHandler _)
+  val subRoutingHandler = controllerAs[SubResource[_]].map( _subRoutingHandler(_) _).getOrElse(_defaultSubRoutingHandler _)
+
+  def routes = new AbstractPartialFunction[RequestHeader, Handler] {
+    override def applyOrElse[A <: RequestHeader, B>: Handler]( requestHeader: A, default: A => B) = {
+      if(requestHeader.path.startsWith(_prefix)) {
+        val path = requestHeader.path.drop(_prefix.length())
+        val method = requestHeader.method
+
+        val res = (method, path) match {
+          case (_,        SubResourceExpression(subPrefix, id, subPath)) => subRoutingHandler(requestHeader, subPrefix, id, subPath)
+          case ("GET",    "" | "/")         => listRoutingHandler()
+          case ("GET",    IdExpression(id)) => getRoutingHandler(id)
+          case ("POST",   "" | "/")         => postRoutingHandler()
+          case ("PUT",    IdExpression(id)) => putRoutingHandler(id)
+          case ("DELETE", IdExpression(id)) => deleteRoutingHandler(id)
+          case ("PATCH",  IdExpression(id)) => patchRoutingHandler(id)
+          case _  => None
         }
-        case _ => default(requestHeader)
+        res.getOrElse(default(requestHeader))
+      } else {
+        default(requestHeader)
       }
     }
-    def withId[A <: RequestHeader, B>: Handler]( id: Id, requestHeader: A, default: A => B): B
-    def withoutId[A <: RequestHeader, B>: Handler](requestHeader: A, default: A => B): B = {
-      default(requestHeader)
-    }
-  }
 
-  class GetRoutingHandler[Id](val resource: ResourceRead[Id]) extends IdRoutingHandler[Id, ResourceRead[Id]] {
-    def withId[A <: RequestHeader, B>: Handler]( id: Id, requestHeader: A, default: A => B) = { resource.get(id) }
-    override def withoutId[A <: RequestHeader, B>: Handler](requestHeader: A, default: A => B) = { resource.list }
-  }
-
-  class PostRoutingHandler(val resource: ResourceAction) extends RoutingHandler() {
-    def isDefined(requestHeader: RequestHeader) = true
-    def apply[A <: RequestHeader, B>: Handler]( requestHeader: A, default: A => B): B = {
-      requestHeader.path.drop(_prefix.length()) match {
-        case "" | "/" => resource.post
-        case _ => default(requestHeader)
-      }
-    }
-  }
-
-  class PutRoutingHandler[Id](val resource: ResourceOverwrite[Id]) extends IdRoutingHandler[Id, ResourceOverwrite[Id]] {
-    def withId[A <: RequestHeader, B>: Handler]( id: Id, requestHeader: A, default: A => B) = { resource.put(id) }
-  }
-
-  class DeleteRoutingHandler[Id](val resource: ResourceUpdate[Id]) extends IdRoutingHandler[Id, ResourceUpdate[Id]] {
-    def withId[A <: RequestHeader, B>: Handler]( id: Id, requestHeader: A, default: A => B) = { resource.delete(id) }
-  }
-
-  class PatchRoutingHandler[Id](val resource: ResourceUpdate[Id]) extends IdRoutingHandler[Id, ResourceUpdate[Id]] {
-    def withId[A <: RequestHeader, B>: Handler]( id: Id, requestHeader: A, default: A => B) = { resource.update(id) }
-  }
-
-  class SubRoutingHandler[Id](val resource: SubResource[Id]) extends RoutingHandler {
-    def isDefined(requestHeader: RequestHeader) = true
-    def apply[A <: RequestHeader, B>: Handler]( requestHeader: A, default: A => B): B = {
-      requestHeader.path.drop(_prefix.length()) match {
-        case SubResourceExpression(subPrefix, sid, subResource) => {
-          for {
-            f <- resource.subResources.get(subResource)
-            id <- resource.fromId(sid)
-            res <- Router.Include {
-              val router = new RestRouter[Controller]{ val controller = f(id) }
-              router.setPrefix(requestHeader.path.take(_prefix.length()+subPrefix.length()))
-              router
-            }.unapply(requestHeader)
-          } yield res
-        }.getOrElse(default(requestHeader))
-
-        case _ => default(requestHeader)
-      }
-    }
-  }
-
-  def findRoutingHandler(requestHeader: RequestHeader): RoutingHandler = {
+    def isDefinedAt(requestHeader: RequestHeader): Boolean = {
       if(requestHeader.path.startsWith(_prefix)) {
         val path = requestHeader.path.drop(_prefix.length())
         val method = requestHeader.method
 
         (path, method, controller) match {
-          case (SubResourceExpression(_, _, _), _, c:SubResource[_]) => new SubRoutingHandler(c)
-          case (_, "GET", c:ResourceRead[_]) => new GetRoutingHandler(c)
-          case (_, "POST", c:ResourceAction) => new PostRoutingHandler(c)
-          case (_, "PUT", c: ResourceOverwrite[_]) => new PutRoutingHandler(c)
-          case (_, "DELETE", c:ResourceUpdate[_]) => new DeleteRoutingHandler(c)
-          case (_, "PATCH", c:ResourceUpdate[_]) => new PatchRoutingHandler(c)
-          case _     => DefaultRoutingHandler
+          case (SubResourceExpression(_, _, _), _, c:SubResource[_]) => true
+          case (_, "GET", c:ResourceRead[_]) => true
+          case (_, "POST", c:ResourceAction) => true
+          case (IdExpression(_), "PUT", c: ResourceOverwrite[_]) => true
+          case (IdExpression(_), "DELETE", c:ResourceUpdate[_]) => true
+          case (IdExpression(_), "PATCH", c:ResourceUpdate[_]) => true
+          case _     => false
         }
       } else {
-        DefaultRoutingHandler
+        false
       }
-  }
-
-  def routes = new AbstractPartialFunction[RequestHeader, Handler] {
-    def isDefinedAt(requestHeader: RequestHeader): Boolean = {
-      findRoutingHandler(requestHeader).isDefined(requestHeader)
-    }
-
-    override def applyOrElse[A <: RequestHeader, B>: Handler]( requestHeader: A, default: A => B) = {
-      findRoutingHandler(requestHeader)(requestHeader, default)
     }
   }
 }
