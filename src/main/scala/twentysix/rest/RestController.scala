99d96e2acf0f41bf9fa5ba7d7c7f190f1ce84e84
Olivier Samyn <olivier@26lights.com>
2013-12-18 11:34:41 +0100

    Add documentation and redifne some shortcut traits

--- src/main/scala/twentysix/rest/RestController.scala
+++ src/main/scala/twentysix/rest/RestController.scala
@@ -8,6 +8,9 @@ import play.api.mvc.Handler
 import play.api.mvc.Controller
 import play.api.Logger
 
+/**
+ * Define the conversion from an url id to a real object
+ */
 trait IdentifiedResource[Id] {
   def toNumber[N](id: String, f: String => N): Option[N] = {
     import scala.util.control.Exception._
@@ -21,28 +24,45 @@ trait IdentifiedResource[Id] {
 
 }
 
+/**
+ * Respond to HTTP GET method
+ */
 trait ResourceRead[Id] extends IdentifiedResource[Id]{
   def get(id: Id): EssentialAction
   def list(): EssentialAction
 }
 
+/**
+ * Respond to HTTP PUT method
+ */
 trait ResourceOverwrite[Id] extends IdentifiedResource[Id]{
   def put(id: Id): EssentialAction
 }
 
+/**
+ * Respond to HTTP POST method
+ */
 trait ResourceCreate {
   def create(): EssentialAction
 }
 
+/**
+ * Respond to HTTP DELETE method
+ */
 trait ResourceDelete[Id] extends IdentifiedResource[Id]{
   def delete(id: Id): EssentialAction
 }
 
+/**
+ * Respond to HTTP PATCH method
+ */
 trait ResourceUpdate[Id] extends IdentifiedResource[Id]{
   def update(id: Id): EssentialAction
 }
 
-
+/**
+ * Define link to other resources accessible via a sub paths
+ */
 trait SubResource[Id] extends IdentifiedResource[Id]{
   def subResources: Map[String, RestAction[Id]]
 }
@@ -52,8 +72,13 @@ trait RestController[Id] extends Controller with IdentifiedResource[Id]
 trait RestReadController[Id] extends Controller with IdentifiedResource[Id]
                                                   with ResourceRead[Id]
 
+trait RestRWController[Id] extends Controller with IdentifiedResource[Id]
+                                               with ResourceCreate
+                                               with ResourceRead[Id]
+                                               with ResourceUpdate[Id]
+
 trait RestCrudController[Id] extends Controller with IdentifiedResource[Id]
                                                  with ResourceCreate
                                                  with ResourceRead[Id]
                                                  with ResourceDelete[Id]
-                                                 with ResourceUpdate[Id]
+                                                 with ResourceOverwrite[Id]
