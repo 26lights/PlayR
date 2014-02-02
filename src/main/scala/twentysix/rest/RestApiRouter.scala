a38d7e3144dea1eb6e08bf7c7a3d35d15d3e4dac
Olivier Samyn <code@oleastre.be>
2014-02-03 00:40:46 +0100

    Implement a simple play router.

--- src/main/scala/twentysix/rest/RestApiRouter.scala
+++ src/main/scala/twentysix/rest/RestApiRouter.scala
@@ -6,56 +6,26 @@ import scala.runtime.AbstractPartialFunction
 import scala.language.implicitConversions
 import play.api.Logger
 
-trait ApiRouter extends RestRouter{
+trait ApiRouter extends RestRouter with SimpleRouter{
   def routeMap: Map[String, RestRouter]
-
-  protected var _prefix: String =""
 
   def routeResources(root: String) = routeMap.flatMap{
     case (path, router) => router.routeResources(s"$root/$path")
   }.toSeq
 
-  def setPrefix(newPrefix: String) = {
-    _prefix = newPrefix
-  }
-
-  def prefix = _prefix
-  def documentation = Nil
-
   private val SubPathExpression = "^(/([^/]+)).*$".r
 
-  def routes = new AbstractPartialFunction[RequestHeader, Handler] {
-    override def applyOrElse[A <: RequestHeader, B>: Handler]( requestHeader: A, default: A => B) = {
-      if(requestHeader.path.startsWith(_prefix)) {
-        val path = requestHeader.path.drop(_prefix.length())
-
-        path match {
-          case SubPathExpression(subPrefix, subPath) => {
-            routeMap.get(subPath).flatMap{ router =>
-              Router.Include {
-                router.setPrefix(prefix+subPrefix)
-                router
-              }.unapply(requestHeader)
-            }.getOrElse(default(requestHeader))
-          }
-          case _ => default(requestHeader)
+  def routeRequest(requestHeader: RequestHeader, path: String, method: String) = {
+    path match {
+      case SubPathExpression(subPrefix, subPath) => {
+        routeMap.get(subPath).flatMap{ router =>
+          Router.Include {
+            router.setPrefix(prefix+subPrefix)
+            router
+          }.unapply(requestHeader)
         }
-      } else {
-        default(requestHeader)
       }
-    }
-
-    def isDefinedAt(requestHeader: RequestHeader): Boolean = {
-      if(requestHeader.path.startsWith(_prefix)) {
-        val path = requestHeader.path.drop(_prefix.length())
-
-        path match {
-          case SubPathExpression(subPath) if routeMap contains subPath => true
-          case _ => false
-        }
-      } else {
-        false
-      }
+      case _ => None
     }
   }
 }
---
+++ src/main/scala/twentysix/rest/SimpleRouter.scala
@@ -0,0 +1,35 @@
+package twentysix.rest
+
+import play.core.Router
+import scala.runtime.AbstractPartialFunction
+import play.api.mvc.RequestHeader
+import play.api.mvc.Handler
+
+trait SimpleRouter extends Router.Routes{
+  protected var _prefix: String =""
+
+  def setPrefix(newPrefix: String) = {
+    _prefix = newPrefix
+  }
+
+  def prefix = _prefix
+  def documentation = Nil
+
+  def routeRequest(header: RequestHeader, path: String, method: String): Option[Handler]
+  
+  def routes = new AbstractPartialFunction[RequestHeader, Handler] {
+    override def applyOrElse[A <: RequestHeader, B>: Handler]( requestHeader: A, default: A => B) = {
+      if(requestHeader.path.startsWith(_prefix)) {
+        val path = requestHeader.path.drop(_prefix.length())
+        val method = requestHeader.method
+        routeRequest(requestHeader, path, method).getOrElse(default(requestHeader))
+      } else {
+        default(requestHeader)
+      }
+    }
+
+    def isDefinedAt(requestHeader: RequestHeader): Boolean = {
+      requestHeader.path.startsWith(_prefix)
+    }
+  }
+}
